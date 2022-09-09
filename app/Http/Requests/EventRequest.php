e727c58effb607e7709ebedd9c9d1526252b46e2
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2022-09-09 16:02:54 +0200

    Create EventRequest.php

---
+++ app/Http/Requests/EventRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Event;
+use Illuminate\Foundation\Http\FormRequest;
+
+class EventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $event = Event::find($this->input('id'));
+
+        return $event && $this->user()->can('can_edit_event', [auth()->user(), $event,'request']);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'invited_id' => 'required|integer',
+            'type_id' => 'required|integer',
+            'title' => 'required|string',
+            'date' => 'required|date',
+        ];
+    }
+}
